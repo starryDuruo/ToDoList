391ec17788953841e5ec453ea440190d732e493f
Sige Wang <wangfiz@bc.edu>
2026-03-08 22:37:13 -0400

    after Ch. 5.6

---
+++ ToDoList/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  ToDoList
+//
+//  Created by Wang Sige on 3/8/26.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,8 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Wang Sige on 3/8/26.
+//
+
+import Foundation
